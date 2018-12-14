22ed482a9fa3b866c71696796625c1b5033f9fc8
Saifee D <saifee.dalal@gmail.com>
2018-12-13 20:54:43 -0600

    team page changes

--- app.py
+++ app.py
@@ -53,16 +53,15 @@ t = Twitter(
     auth=OAuth(twitter_token, twitter_token_secret, twitter_consumer_key, twitter_consumer_secret))
 
 
-@app.route('/getFlightPrediction/<airline>/<depAirport>/<arrAirport>/<calendarDay>/<weekDay>/<hour>')
-def getPrediction(airline,depAirport,arrAirport,calendarDay,weekDay,hour):
+@app.route('/getFlightPrediction/<airline>/<month>/<calendarDay>/<weekDay>/<hour>')
+def getPrediction(airline,month,calendarDay,weekDay,hour):
     print("Inside getPrediction")
     print("Airline :" +airline)
-    print("Dep Airport :" +depAirport)
-    print("Arr Airport :" +arrAirport)
+    print("Month :" +month)
     print("CalendarDay :" +calendarDay)
     print("WeekDay :" +weekDay)
     print("Hour :" +hour)
-    response = getModelOutput(airline,depAirport,arrAirport,calendarDay,weekDay,hour)
+    response = getModelOutput(airline,month,calendarDay,weekDay,hour)
     print(response)
     return jsonify(response)
 
@@ -84,13 +83,7 @@ def getTweets(searchKeyword):
     print(response)
     return jsonify(response)
 
-def getModelOutput(airline,depAirport,arrAirport,calendarDay,weekDay,hour):
-    print("Inside getModelOutput")
-    if airline == 'null':
-        print ("Use model without airline")
-    else:
-        print ("Use model with airline")
-    #hardcoding response below. Ideally the model would predict a 0 or 1
+def getModelOutput(airline,month,calendarDay,weekDay,hour):
     response = 0
     return response
 
--- static/js/statslogic.js
+++ static/js/statslogic.js
@@ -53,6 +53,9 @@ submit_btn.on("click", function() {
         var calendarDay = d.getDate();
         console.log(`Calendar Day is ${calendarDay}`);
 
+        var month = d.getMonth() + 1;
+        console.log(`Month is ${month}`);
+
         var weekDay = d.getDay();
 
         if (weekDay == 0)
@@ -63,17 +66,17 @@ submit_btn.on("click", function() {
         var hour = 0;
 
         if (time == "Early Morning (00:00 - 06:59 CST)")
-          hour = 06;
+          hour = 0600;
         else if (time == "Morning (07:00 - 11:59 CST)")
-          hour = 11;
+          hour = 1100;
         else if (time == "Afternoon (12:00 - 15:59 CST)")
-          hour = 15;
+          hour = 1500;
         else if (time == "Evening (16:00 - 18:59 CST)")
-          hour = 18;
+          hour = 1800;
         else if (time == "Late Evening (19:00 - 20:59 CST)")
-          hour = 20;
+          hour = 2000;
         else if (time == "Night (21:00 - 23:59 CST)")
-          hour = 23;
+          hour = 2300;
         else
           hour = 16;
 
@@ -82,43 +85,47 @@ submit_btn.on("click", function() {
         var airline_code = 'null';
 
         if (airline == 'Alaska')
-          airline_code = 'AS'
-        else if (airline == 'Allegiant')
-          airline_code = 'G4'
+          airline_code = 1
+        // else if (airline == 'Allegiant')
+        //   airline_code = 'G4'
         else if (airline == 'American')
-          airline_code = 'AA'
-        else if (airline == 'American Eagle')
-          airline_code = 'MQ'
+          airline_code = 0
+        // else if (airline == 'American Eagle')
+        //   airline_code = 'MQ'
         else if (airline == 'Atlantic Southeast')
-          airline_code = 'EV'
-        else if (airline == 'Comair')
-          airline_code = 'OH'
+          airline_code = 4
+        // else if (airline == 'Comair')
+        //   airline_code = 'OH'
         else if (airline == 'Delta')
-          airline_code = 'DL'
+          airline_code = 3
         else if (airline == 'Frontier')
-          airline_code = 'F9'
+          airline_code = 5
         else if (airline == 'Hawaiian')
-          airline_code = 'HA'
+          airline_code = 6
         else if (airline == 'Jetblue')
-          airline_code = 'B6'
-        else if (airline == 'Mesa')
-          airline_code = 'YV'
-        else if (airline == 'Midwest')
-          airline_code = 'YX'
-        else if (airline == 'Pinnacle')
-          airline_code = '9E'
+          airline_code = 2
+        // else if (airline == 'Mesa')
+        //   airline_code = 'YV'
+        // else if (airline == 'Midwest')
+        //   airline_code = 'YX'
+        // else if (airline == 'Pinnacle')
+        //   airline_code = '9E'
         else if (airline == 'Skywest')
-          airline_code = 'OO'
+          airline_code = 8
         else if (airline == 'Southwest')
-          airline_code = 'WN'
+          airline_code = 11
         else if (airline == 'Spirit')
-          airline_code = 'NK'
+          airline_code = 7
         else if (airline == 'United')
-          airline_code = 'UA'
+          airline_code = 9
         else if (airline == 'Virgin')
-          airline_code = 'VX'
+          airline_code = 10
+        else 
+          airline_code = 12
+
+          console.log(`Airline code is ${airline_code}`);
         
-        var url = "/getFlightPrediction/"+airline_code+"/"+depAirport+"/"+arrAirport+"/"+calendarDay+"/"+weekDay+"/"+hour;
+        var url = "/getFlightPrediction/"+airline_code+"/"+month+"/"+calendarDay+"/"+weekDay+"/"+hour;
         console.log("URL", url);
         //Calling the getYelpReview method to fetch Yelp Reviews
         d3.json(url,function(data) {
